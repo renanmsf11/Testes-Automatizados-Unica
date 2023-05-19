70050c329136d4b6d98cdc366b43f2aa9d51ef4a
Renan Marques <renanmarques33@gmail.com>
2023-05-19 06:38:48 -0300

    Update AdicionarAoCarrinho.spec.cy.js

--- cypress/e2e/Testes/AdicionarAoCarrinho.spec.cy.js
+++ cypress/e2e/Testes/AdicionarAoCarrinho.spec.cy.js
@@ -1,6 +1,6 @@
 describe('Fluxo de adicionar item ao carrinho', () =>  {
 
-    it('se é possível alterar a quantidade do item através do botão de dropdown no carrinho', () => {
+    it('Valida se é possível alterar a quantidade do item através do botão de dropdown no carrinho', () => {
         cy.fixture('elements').then((el) => {
             cy.AdicionarProdutoAoCarrinho(el.produto)
             
